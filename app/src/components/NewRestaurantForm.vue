a49c87f0ec67f2b9311cd2a6daecd5e14b2e8986
Joe Wilson <joe.wilson@jwils.xyz>
2024-12-24 18:33:34 -0500

    Update `NewRestaurantForm` to use `<script setup>` with `defineEmits`, move event handlers into setup, focus first field on mount

--- app/src/components/NewRestaurantForm.vue
+++ app/src/components/NewRestaurantForm.vue
@@ -1,18 +1,30 @@
-<script>
+<script lang="ts" setup>
+import { statusList } from '@/constants'
+import { type Restaurant } from '@/types'
 import { v4 as uuidv4 } from 'uuid'
+import { onMounted, reactive, ref } from 'vue'
 
-export default {
-  emits: ['add-new-restaurant', 'cancel-new-restaurant'],
-  data: () => ({
-    newRestaurant: {
-      id: uuidv4(),
-      name: '',
-      address: '',
-      website: '',
-      status: 'Want to Try',
-    },
-  }),
+type Emits = {
+  'add-new-restaurant': [restaurant: Restaurant]
+  'cancel-new-restaurant': []
 }
+
+const emit = defineEmits<Emits>()
+const addNewRestaurant = () => emit('add-new-restaurant', newRestaurant)
+const cancelNewRestaurant = () => emit('cancel-new-restaurant')
+
+const newRestaurant = reactive<Restaurant>({
+  id: uuidv4(),
+  name: '',
+  address: '',
+  website: '',
+  status: 'Want to Try',
+})
+
+const elNameInput = ref<HTMLInputElement | null>(null);
+onMounted(() => {
+  elNameInput.value?.focus();
+})
 </script>
 
 <template>
@@ -22,8 +34,7 @@ export default {
         <label for="name" class="label">Name</label>
         <div class="control">
           <input
-            :value="newRestaurant.name"
-            @keyup.space="updateName"
+            v-model="newRestaurant.name"
             type="text"
             class="input is-large"
             placeholder="Beignet and the Jets"
@@ -42,7 +53,7 @@ export default {
         <label for="status" class="label">Status</label>
         <div class="select">
           <select v-model="newRestaurant.status" id="status">
-            <option v-for="status in restaurantStatusList" :value="status" :key="`option-${status}`">
+            <option v-for="status in statusList" :value="status" :key="`option-${status}`">
               {{ status }}
             </option>
           </select>
@@ -50,8 +61,8 @@ export default {
       </div>
       <div class="field">
         <div class="buttons">
-          <button @click="$emit('add-new-restaurant', newRestaurant)" class="button is-success">Create</button>
-          <button @click="$emit('cancel-new-restaurant')" class="button is-light">Cancel</button>
+          <button @click="addNewRestaurant" class="button is-success">Create</button>
+          <button @click="cancelNewRestaurant" class="button is-light">Cancel</button>
         </div>
       </div>
     </div>
