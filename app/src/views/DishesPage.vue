1e6449cf822b4d65ce2a950c8ae21026b09cf6b2
Joe Wilson <joe.wilson@jwils.xyz>
2024-12-24 19:49:56 -0500

    Create `DishStore` with code previously in `DishesPage`

--- app/src/views/DishesPage.vue
+++ app/src/views/DishesPage.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { useDishStore } from '@/stores/DishStore'
 import type { Dish } from '@/types'
 import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
@@ -6,23 +7,7 @@ import DishCard from '../components/DishCard.vue'
 import NewDishForm from '../components/NewDishForm.vue'
 import SideMenu from '../components/SideMenu.vue'
 
-const dishList = ref<Dish[]>([
-  {
-    id: '7d9f3f17-964a-4e82-98e5-ecbba4d709a1',
-    name: 'Ghost Pepper Poppers',
-    status: 'Want to Try',
-  },
-  {
-    id: '5c986b74-fa02-4a22-98f2-b1ff3559e85e',
-    name: 'A Little More Chowder Now',
-    status: 'Recommended',
-  },
-  {
-    id: 'c113411d-1589-414f-a283-daf7eedb631e',
-    name: 'Full Laptop Battery',
-    status: 'Do Not Recommend',
-  },
-])
+const dishStore = useDishStore()
 
 const filterText = ref('')
 const updateFilterText = (event: InputEvent) => {
@@ -32,11 +17,11 @@ const updateFilterText = (event: InputEvent) => {
 }
 
 const filteredDishList = computed((): Dish[] => {
-  return dishList.value.filter((dish) => {
+  return dishStore.list.filter((dish) => {
     if (dish.name) {
       return dish.name.toLowerCase().includes(filterText.value.toLowerCase())
     } else {
-      return dishList.value
+      return dishStore.list
     }
   })
 })
@@ -44,13 +29,8 @@ const filteredDishList = computed((): Dish[] => {
 const numberOfDishes = computed(() => filteredDishList.value.length)
 
 const addDish = (payload: Dish) => {
-  dishList.value.push(payload)
+  dishStore.addDish(payload)
   hideForm()
-}
-const deleteDish = (payload: Dish) => {
-  dishList.value = dishList.value.filter((dish) => {
-    return dish.id !== payload.id
-  })
 }
 
 const showNewForm = ref(false)
@@ -108,7 +88,7 @@ onMounted(() => {
         <!-- Display Results -->
         <div v-else class="columns is-multiline">
           <div v-for="item in filteredDishList" class="column is-full" :key="`item-${item}`">
-            <DishCard :dish="item" @delete-dish="deleteDish" />
+            <DishCard :dish="item" @delete-dish="dishStore.deleteDish" />
           </div>
         </div>
       </div>
